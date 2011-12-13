4fb584726e1db5c1bc42ecd7b3e33d2b3565af1c
zé <jose.filho@gonow.com.br>
2011-12-13 16:19:48 -0200

    Creating textarea for text types and required message for fields

--- templates/model.php
+++ templates/model.php
@@ -1,7 +1,11 @@
 <?php
 
+/**
+ * <%=@model.capitalize%> Model.
+ */
 class <%=@model.capitalize%>_model extends CI_Model {
 
+	# save $data on '<%=@model.downcase%>'
 	function save($data) {
 		<% @attributes.each { |f| %>
 		$this->db->set('<%= f.name %>', $data['<%= f.name %>']);<% } %>
@@ -18,6 +22,7 @@ class <%=@model.capitalize%>_model extends CI_Model {
 		return $this->db->affected_rows();
 	}
 
+	# retrives $data from '<%=@model.downcase%>'
 	function find($id = NULL) {
 		if($id != NULL) {
 			$this->db->where('id', $id);
@@ -27,6 +32,7 @@ class <%=@model.capitalize%>_model extends CI_Model {
 		}
 	}
 
+	# destroy $data from  '<%=@model.downcase%>'
 	function destroy($id) {
 		$this->db->where('id', $id);
 		$this->db->delete('<%=@model.downcase%>');
--- templates/view_create.php
+++ templates/view_create.php
@@ -1,10 +1,14 @@
 	<h1>Create</h1>
 	<?php echo form_open('<%=@model.downcase%>/save'); ?>
+		<div class="error"><?php echo validation_errors(); ?></div>
 		<input type="hidden" name="id" value="<?php echo isset($<%=@model.downcase%>)?$<%=@model.downcase%>->id:''?>"/>
 		<% @attributes.each { |f| %>
 		<p>
-			<label for="<%= f.name %>"><%= f.name %>:</label><br/>
-			<input type="text" name="<%= f.name %>" value="<?php echo isset($<%=@model.downcase%>)?$<%=@model.downcase%>-><%= f.name %>:''?>"/>
+			<label for="<%= f.name %>"><%= f.name %>:</label><br/><% if f.type == 'string' %>
+			<input type="text" name="<%= f.name %>" value="<?php echo isset($<%=@model.downcase%>)?$<%=@model.downcase%>-><%= f.name %>:''?>"/><% end %>
+			<% if f.type == 'text' %>
+			<textarea name="<%= f.name %>"><?php echo isset($<%=@model.downcase%>)?$<%=@model.downcase%>-><%= f.name %>:''?></textarea><% end %>
+
 		</p>
 		<% } %>
 		<input type="submit" value="Create"/>
