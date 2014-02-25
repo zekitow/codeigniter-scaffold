adf2335da2c2029bc1e42381a174027925141bfc
José Ribeiro Filho <zekitow@gmail.com>
2014-02-25 16:10:39 -0300

    Removed user_guide from ci.zip

--- spec/codeigniter_scaffold/command/init_spec.rb
+++ spec/codeigniter_scaffold/command/init_spec.rb
@@ -6,13 +6,11 @@ describe CodeigniterScaffold::Command::Init do
     before(:all) { CodeigniterScaffold::Command::Init.new.run(nil) }
 
     after(:all) do
-      unziped = ["application","index.php","system","user_guide"]
+      unziped = ["application","index.php","system"]
       unziped.each { |f| FileUtils.rm_rf Dir.glob(f) }
     end
 
     it { File.should be_directory(File.join(Dir.pwd,"/application")) }
     it { File.should be_directory(File.join(Dir.pwd,"/system")) }
-    it { File.should be_directory(File.join(Dir.pwd,"/user_guide")) }
-    it { File.should exist(File.join(Dir.pwd,"/user_guide")) }
   end
 end
